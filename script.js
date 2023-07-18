caddcb118203f101280f8091ddbf3e7d20b7cbfc
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-18 13:04:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
 var googleMapsApiKey = "";
 var openWeatherApiKey = "http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={8686e0fe4732b6b364f3c95d6dfcf09c}";
 
+// Backup for Google Maps - API Key from Bing Maps then Mapquest/Radar
+// var mapquestApiKey = "prj_test_pk_ef08dfd3ed0a8748c5d83db178b8283bcdf5c539";
+var bingMapsApiKey = "AjuFMgDNUeVWcHGU633JdkzIGhbOjzM6-ruaD9QyJr4RPe4AHW-qEug1BK1URiom";
+
+// Backup for Open Weather - API Key from RapidAPI
+var rapidApiKey = "b9b932d13amshd7a22bc43c9b9cep19e1b4jsn54e36226b53c";
+
 // variable search area and button
 var searchText = document.querySelector("#search-box");
 var searchBtn = document.querySelector("#searchButton");
 var searchedCities = document.querySelector("#searchedCities");
+
 var cityName = document.querySelector("#city-name");
 var airEl = document.querySelector("#air");
-
-
 
 // variable to hold map container
 var mapContainer = document.querySelector(".map-container");
@@ -22,99 +28,98 @@ var mapEl = document.querySelector("#mapone");
 
 // get lat and long
 
-
 // search button event listener
+
 searchBtn.addEventListener("click", function (e) {
-    // preventDefault when button is in a form
-    // e.preventDefault();
-    console.log("search button clicked");
     console.log(searchText.value);
     citySearch(searchText.value);
 });
+
+//googlemap
+function myMap(lat,lon) {
+  var mapProp = {
+      center: new google.maps.LatLng(lat, lon),
+      zoom: 8,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
 // function for search
-function citySearch(searchResults) {
+function citySearch(cityName) {
     var airQuality;
-    if (searchResults === "Good" || searchResults === "Fair" || searchResults === "Moderate" || searchResults === "Poor" || searchResults === "Very Poor") {
-        airQuality = searchResults;
-        return;
-
-    }
-
+    // if (searchResults === "Good" || searchResults === "Fair" || searchResults === "Moderate" || searchResults === "Poor" || searchResults === "Very Poor") {
+    //     airQuality = searchResults;
+    //     return;
+    // }
 
     var apiKeyAir = "8686e0fe4732b6b364f3c95d6dfcf09c";
     var apiUrlGeo = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKeyAir;
-    console.log("inside citySearch", searchResults);
+    // console.log("inside citySearch", searchResults);
+
+    // only need function inside fetch if you need to create a function
     fetch(apiUrlGeo)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            console.log(response);
-            // getAirQuality(response);
-            var {lat, lon, name} = response[0];
-            console.log(lat, lon);
-            getQuality(lat, lon, name);
-        });
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (response) {
+        console.log(response);
+        var {lat, lon, name} = response[0];
+        getQuality(lat, lon, name);
+        myMap(lat, lon);
+    });
 }
+
 // Fetch the air quality
 function getQuality(lat, lon, name) {
     var apiKeyAir = "8686e0fe4732b6b364f3c95d6dfcf09c";
     var apiUrlAir = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKeyAir}`;
     fetch(apiUrlAir)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            console.log("air response", response);
-            // getAirQuality(response);
-            var {aqi} = response.list[0].main;
-            console.log(aqi);
-            renderAirQuality(aqi, name);
-        });
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (response) {
+        var {aqi} = response.list[0].main;
+        renderAirQuality(aqi, name);
+    });
+        };
 
 
-
-        // display an icon to represent good, fair, moderate, poor, very poor and we create our own scale
 
 function getAirQuality(response) {
-    // figure out variables and array for this
-    // use this as guide
-    // var cityName = response.city.name;
-    // var getTemp = response.list[0].main.temp;
-    // var getWind = response.list[0].wind.speed;
-    // var getHumid = response.list[0].main.humidity;
-    // city.textContent = cityName;
-    // tempEL.textContent = getTemp + "°F";
-    // windEl.textContent = getWind + " mph";
-    // humidEl.textContent = getHumid + "%";
-}
+//     // figure out variables and array for this
+//     // use this as guide
+//     // var cityName = response.city.name;
+//     // var getTemp = response.list[0].main.temp;
+//     // var getWind = response.list[0].wind.speed;
+//     // var getHumid = response.list[0].main.humidity;
+//     // city.textContent = cityName;
+//     // tempEL.textContent = getTemp + "°F";
+//     // windEl.textContent = getWind + " mph";
+//     // humidEl.textContent = getHumid + "%";
+        }
 
-}
+// display an icon to represent good, fair, moderate, poor, very poor and we create our own scale
 
 function renderAirQuality(aqi, cityName) {
-  var nameOfCity = document.createElement("h2");
-  var airQualityEl = document.createElement("h3");
-  nameOfCity.textContent = cityName;
-  var airQuality;
-  if (aqi == 1) {
-    airQuality = "Good";
-  }
-  if (aqi == 2) {
-    airQuality = "Fair";
-  }
-  if (aqi == 3) {
-    airQuality = "Moderate";
-  }
-  if (aqi == 4) {
-    airQuality = "Poor";
-  }
-  if (aqi == 5) {
-    airQuality = "Very Poor";
-  }
-  airQualityEl.textContent = airQuality;
-  airEl.append(nameOfCity, airQualityEl);
-  console.log("aqi", aqi);
-  console.log(cityName);
-;}
-
-// Is client-side storage the same as local storage?
+    var nameOfCity = document.createElement("h2");
+    var airQualityEl = document.createElement("h3");
+    nameOfCity.textContent = cityName;
+    var airQuality;
+    if (aqi == 1) {
+      airQuality = "Air quality is good" + " 🔵";
+    }
+    if (aqi == 2) {
+      airQuality = "Air quality is fair" + " 🟢";
+    }
+    if (aqi == 3) {
+      airQuality = "Air quality is moderate" + " 🟡";
+    }
+    if (aqi == 4) {
+      airQuality = "Air quality is poor" + " 🟠";
+    }
+    if (aqi == 5) {
+      airQuality = "Air quality is very poor" + " 🔴";
+    }
+    airQualityEl.textContent = airQuality;
+    airEl.append(nameOfCity, airQualityEl);
+}
